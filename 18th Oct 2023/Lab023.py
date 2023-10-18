44a1b5de1929bba6e03f8a7409614ec3a36966e8
pandey192 <pandeyshivam192@gmail.com>
2023-10-19 01:17:40 +0530

    18th oct class2.

---
+++ 18th Oct 2023/Lab023.py
@@ -0,0 +1,9 @@
+#pass statement
+#the pass statement does nothing.it can be used when a statement is required syentaticlly but programs required no action
+
+for i in range(1,10):
+    if i == 5:
+        pass
+    else:
+        print(i)
+    
---
+++ 18th Oct 2023/Lab024.py
@@ -0,0 +1,7 @@
+#Continue
+
+for num in range(1,10):
+    if num % 2 == 0:
+        print("found even number")
+        continue
+    print(num)
