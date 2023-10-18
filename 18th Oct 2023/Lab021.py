b0ce392e9d8c280b35a55a97a57ebb79c97261dd
pandey192 <pandeyshivam192@gmail.com>
2023-10-19 01:00:41 +0530

    18th oct work.

---
+++ 18th Oct 2023/Lab021.py
@@ -0,0 +1,10 @@
+#Break and continue with the loops
+#Break
+#1 to 10 -> when value = 5 -> 1,2,3,4,5
+#put the debug point when see condition
+count = 0
+while count <=10:
+    print(count)
+    if count >=5:
+        break
+    count = count +1
