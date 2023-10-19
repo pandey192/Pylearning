9edd4dcf35cd6142c70f5ae3fdc1f52860a8c653
pandey192 <pandeyshivam192@gmail.com>
2023-10-20 00:57:42 +0530

    18th task.

---
+++ 18th Oct 2023/Task_Program_1.py
@@ -0,0 +1,9 @@
+#Research What is Fibonacci series and Factorial.
+#Take an input and print the series.
+
+num = int(input("enter the number"))
+
+for num in range(num):
+    num = num +1
+print(num)
+
---
+++ 18th Oct 2023/Task_Program_2.py
@@ -0,0 +1,7 @@
+#Example using break to exit a loop when i == 51 while printing the values from 1 to 100
+
+for i in range(1,100):
+    if i == 51:
+        break
+    print(i)
+    
