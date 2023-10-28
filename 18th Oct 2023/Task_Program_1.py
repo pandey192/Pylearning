f3cb405400ed35fcc89cc7cb6ed36c92f0f3c193
pandey192 <pandeyshivam192@gmail.com>
2023-10-28 14:01:58 +0530

    new commit.

--- 18th Oct 2023/Task_Program_1.py
+++ 18th Oct 2023/Task_Program_1.py
@@ -8,9 +8,23 @@
 
 #with seed values : F0 = 0 and F1 = 1.
 
-num = int(input("enter the number"))
+#function for nth Fibonacci number
 
-for num in range(num):
-    num = num +1
-print(num)
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2
+count = 1
+
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
+
+
+
+
+
 
--- 18th Oct 2023/Task_program_3.py
+++ 18th Oct 2023/Task_program_3.py
@@ -1,7 +1,7 @@
-#factorial
+# factorial
 
 num = int(input("Enter a number"))
 x = 1
-for i in range(1,num+1):
-    x = i*x
+for i in range(1, num + 1):
+    x = i * x
     print(x)
