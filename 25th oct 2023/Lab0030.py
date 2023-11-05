0a3e7c14f2362d85427a82803c4b661691c5d696
pandey192 <pandeyshivam192@gmail.com>
2023-11-06 00:19:50 +0530

    new chnages7.

---
+++ 25th oct 2023/Lab0030.py
@@ -0,0 +1,15 @@
+my_list = [1,2,3,3]
+my_list2 = [1,True,"Shivam",12.34]
+
+print("Initial_list",my_list)
+#indexing
+print("Element at index",my_list[0])
+
+#insert()
+my_list.insert(1,"a")
+print("List after inserting 'a' at index 1",my_list)
+
+#remove()
+
+my_list.remove('a')
+print("List after remving 'a'")
